11ad451e04af7b973cab01231a8411e177a1862e
Pradeep Ravichandran <32986647+PradeepRavichandran1811@users.noreply.github.com>
2019-06-12 21:30:44 -0500

    application update

--- application.py
+++ application.py
@@ -1,17 +1,12 @@
 import spotipy
-import sys
-import spotipy.util as util
 from flask import Flask, render_template, request
 from spotipy.oauth2 import SpotifyClientCredentials
 app = Flask(__name__)
-CLIENT_ID = "your_client_id"
-CLIENT_SECRET = "your_secret_key"
-art={'charlie puth':'6VuMaDnrHyPL1p4EHjYLi7','imagine dragons':'53XhwfbYqKCa1cC15pYq2q','bruno mars':'0du5cEVh5yTK9QJze8zA0C','shawn mendes':'7n2wHs1TKAczGzO7Dd2rGr','taylor swift':'06HL4z0CvFAxyc27GXpf02','selena gomez':'0C8ZW7ezQVs4URX5aX7Kqx','maroon 5':'0aL7M6qKDacHCtUUch3AhB',
-     'dua lipa':'6M2wZ9GZgrQXHCFfjv46we','post malone':'246dkjvS1zLTtiykXe5h60','john mayer':'0hEurMDQu99nJRq8pTxO14','ed sheeran':'6eUKZXaKkcviH0Ku9w2n3V',
-     'khalid':'6LuN9FCkKOj5PcnpouEgny','ariana grande':'66CXWjxzNUsdJxJ2JdwvnR','jonas brothers':'7gOdHgIoIKoe4i9Tta6qdD',
-     'drake':'3TVXtAsR1Inumwj472S9r4','cardi b':'4kYSro6naA4h99UJvo89HB','justin bieber':'1uNFoZAHBGtllmzznpCI3s','eminem':'7dGJo4pcD2V6oG8kP0tJRR',
-     }
+CLIENT_ID = "94a53da1882a498b827a9053947aa26d"
+CLIENT_SECRET = "9e32cd0089e94694b927ebd4de46b940"
+
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+@app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -22,26 +17,24 @@ def toptracks():
     if request.method == 'POST':
         artist_ = request.form['name']
         artist_final = artist_.lower()
-    artist_id = art[artist_final]
-    urn +=artist_id
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    artist = sp.artist(urn)
-    # print(artist)
-    artist_name = artist['name']
-    artist_img = artist['images'][0]['url']
-
-    # print("***************Top 10 Tracks******************")
-    top = sp.artist_top_tracks(artist_id, country='US')
-    # count = 0
-    #print(top)
-    i=1
-    songs=[]
-    audio=[]
-    cover=[]
-    for track in top['tracks'][:10]:
-        print(track)
-        songs.append(("Song {}  :".format(i)+" "+track['name'],track['album']['name'],track['uri'],track['album']['images'][0]['url']))
-        i+=1
+    search=sp.search(artist_final, limit=5, offset=0, type='artist', market=None)
+    print(search)
+    if search['artists']['items']!=[]:
+        artist_id=search['artists']['items'][0]['id']
+        urn += artist_id
+        artist = sp.artist(urn)
+        artist_name = artist['name']
+        artist_img = artist['images'][0]['url']
+        top = sp.artist_top_tracks(artist_id, country='US')
+        i=1
+        songs=[]
+        for track in top['tracks'][:10]:
+            print(track)
+            songs.append(("Song {}  :".format(i)+" "+track['name'],track['album']['name'],track['uri'],track['album']['images'][0]['url']))
+            i+=1
+    else:
+        return render_template("error.html")
     return render_template("spotify.html", artist=artist_name, artist_img=artist_img ,songs=songs)
 
 
